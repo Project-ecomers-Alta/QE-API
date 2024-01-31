96343e2f7e0ec354805935f0b255c6bdb86454bb
Sofie Miranti Khairunnisa <sofiemirantikha@gmail.com>
2024-01-31 19:55:56 +0700

    add product feature scenarios

--- src/test/java/starter/ecommerceapp/EcommerceAppResponses.java
+++ src/test/java/starter/ecommerceapp/EcommerceAppResponses.java
@@ -2,5 +2,6 @@ package starter.ecommerceapp;
 
 public class EcommerceAppResponses {
     public static String MESSAGE = "message";
-
+    public static String DATA_CATEGORY = "data[0].category";
+    public static String DATA_PRODUCT_ID = "data.id";
 }
--- src/test/java/starter/utils/Constants.java
+++ src/test/java/starter/utils/Constants.java
@@ -1,10 +1,13 @@
 package starter.utils;
 
+import starter.stepdef.LoginStepDef;
+
 public class Constants {
     public static String DIR = System.getProperty("user.dir");
-    public static String BASE_URL = "http://54.250.172.137:8080";
+    public static String BASE_URL = "https://be20.online";
     public static String JSON_DIR = DIR + "/src/test/resources/JSON";
+    public static String IMAGE_DIR = DIR + "/src/test/resources/image";
     public static String REQ_BODY = JSON_DIR + "/ReqBody/";
     public static String JSON_SCHEMA = JSON_DIR + "/JsonSchema/";
-    public static String BEARER_TOKEN = "GciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MDY2NjI4NzUsInVzZXJJZCI6MTd9.YyMnfjAXIaHnBmCdjL9SAjNnpbNvtFfU8l1JAt21Y2g";
+    public static String BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE3MDY3MDgyMTgsInVzZXJJZCI6MTd9.dF8FR6tgXLpIhOV6QKLcxFbEC5aIVGwKN7QX96-yp44";
 }
